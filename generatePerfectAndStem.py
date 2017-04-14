0568a6783176b7c61b5ecf51b0798bd58a4d6227
edson <soued0311@hotmail.com>
2017-04-14 13:47:01 +0200

    Adding script to generate a CSV list of perfects and their stem

---
+++ generatePerfectAndStem.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import xml.etree.ElementTree as etree
+import glob
+import csv
+
+etree.register_namespace('lod', 'http://www.lod.lu')
+
+perfectAndStemList = []
+
+fileList = glob.glob("XML/*.xml")
+
+def generatePerfectAndStemCSV():
+    for file in fileList:
+        tree = etree.parse(file)
+        root = tree.getroot()
+        for child in root:
+            verbOrNot = child[1][1][0][0].tag
+            if verbOrNot == "{http://www.lod.lu/}CAT-GRAM-VRB":
+                stem = child[1][0].text
+                perfect = child[1][1][0][2].text
+                if len(perfect.strip()) > 0:
+                    perfectAndStemList.append([perfect, stem])
+
+    if perfectAndStemList :
+        with open('perfectAndStem.csv', 'w') as myfile:
+            writer = csv.writer(myfile)
+            writer.writerows(perfectAndStemList)
+
+if __name__ == "__main__":
+    generatePerfectAndStemCSV()
